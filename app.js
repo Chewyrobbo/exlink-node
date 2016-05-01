e1b34873a3a9797e51548ab684744585d7300c63
Entertainment <entertainment@Entertainments-MacBook-Pro.local>
2016-05-01 09:24:15 -0700

    Added file sync

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+#!/usr/local/bin/node
 var SerialPort = require("serialport").SerialPort
+var fs = require('fs');
 var port  = new SerialPort("/dev/cu.usbserial", {
   baudrate: 9600
 });
-
-var command = {}
-command.powerToggle =  new Buffer('082200000000D6','hex');
-
-
+var option = process.argv[2]
+var command;
+fs.readFile('options.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  command = JSON.parse(data);
+});
 port.on('open', function () {
-  port.write(command.powerToggle, function(err, bytesWritten) {
+  port.write(Buffer.from(command[option],'hex'), function(err, bytesWritten) {
     if (err) {
       return console.log('Error: ', err.message);
     }
-console.log("Power Toggled.");
     console.log(bytesWritten, 'bytes written');
     process.exit()
   });
